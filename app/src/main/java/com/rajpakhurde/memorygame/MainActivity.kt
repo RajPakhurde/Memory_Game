d571ed43055c0e0da01e89ef591ff150146c98d4
Raj_Pakhurde <rajofficial4803@gmail.com>
2022-12-19 14:23:06 +0530

    Created menubar and added funtion of resized cards

--- app/src/main/java/com/rajpakhurde/memorygame/MainActivity.kt
+++ app/src/main/java/com/rajpakhurde/memorygame/MainActivity.kt
@@ -4,7 +4,13 @@ import android.animation.ArgbEvaluator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.RadioGroup
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
@@ -18,7 +24,6 @@ import com.rajpakhurde.memorygame.utils.DEFAULT_ICONS
 class MainActivity : AppCompatActivity() {
 
 
-
     private lateinit var clRoot: ConstraintLayout
     private lateinit var rvBoard: RecyclerView
     private lateinit var tvNumMoves: TextView
@@ -26,9 +31,9 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var memoryGame: MemoryGame
     private lateinit var adapter: MemoryBoardAdapter
-    private val boardSize: BoardSize = BoardSize.EASY
+    private var boardSize: BoardSize = BoardSize.EASY
 
-    companion object{
+    companion object {
         private const val TAG = "MainActivity"
     }
 
@@ -41,46 +46,120 @@ class MainActivity : AppCompatActivity() {
         tvNumMoves = findViewById(R.id.tvNumMoves)
         tvNumPairs = findViewById(R.id.tvNumPairs)
 
-        tvNumPairs.setTextColor(ContextCompat.getColor(this,R.color.color_progress_none))
-        memoryGame = MemoryGame(boardSize)
-        adapter = MemoryBoardAdapter(this,boardSize,memoryGame.cards, object: MemoryBoardAdapter.CardClickListener{
-            override fun onCardClicked(position: Int) {
-                updateGameWithFlip(position)
-            }
+       setUpBoard()
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.mi_refresh -> {
+                if(memoryGame.getNumMoves() > 0 && !memoryGame.haveWonGame()){
+                    showAlertDialog("Quit your current game?", null,View.OnClickListener {
+                        setUpBoard()
+                    })
+                }else{
+                    setUpBoard()
+                }
+            }
+            R.id.mi_newSize -> {
+                showNewSizeDialog()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showNewSizeDialog(){
+        val boardSizeView = LayoutInflater.from(this).inflate(R.layout.dialog_board_size,null)
+        val radioGroupSize = boardSizeView.findViewById<RadioGroup>(R.id.radioGroup)
+        when(boardSize){
+            BoardSize.EASY -> radioGroupSize.check(R.id.rbEasy)
+            BoardSize.MEDIUM -> radioGroupSize.check(R.id.rbMedium)
+            BoardSize.HARD -> radioGroupSize.check(R.id.rbHard)
+        }
+        showAlertDialog("Choose new size",boardSizeView,View.OnClickListener {
+            boardSize = when(radioGroupSize.checkedRadioButtonId){
+                R.id.rbEasy ->  BoardSize.EASY
+                R.id.rbMedium -> BoardSize.MEDIUM
+                else -> BoardSize.HARD
+            }
+            setUpBoard()
         })
+    }
+
+    private fun showAlertDialog(title: String, view: View?, positiveClickListener: View.OnClickListener ){
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setView(view)
+            .setNegativeButton("Cancel", null)
+            .setPositiveButton("Ok"){_,_->
+                positiveClickListener.onClick(null)
+            }.show()
+    }
+
+    private fun setUpBoard(){
+        when(boardSize){
+            BoardSize.EASY -> {
+                tvNumMoves.text = "Easy: 4 x 2"
+                tvNumPairs.text = "Pairs: 0 / 4"
+            }
+            BoardSize.MEDIUM ->  {
+                tvNumMoves.text = "Easy: 6 x 3"
+                tvNumPairs.text = "Pairs: 0 / 9"
+            }
+            BoardSize.HARD -> {
+                tvNumMoves.text = "Easy: 6 x 6"
+                tvNumPairs.text = "Pairs: 0 / 12"
+            }
+        }
+        tvNumPairs.setTextColor(ContextCompat.getColor(this, R.color.color_progress_none))
+        memoryGame = MemoryGame(boardSize)
+        adapter = MemoryBoardAdapter(
+            this,
+            boardSize,
+            memoryGame.cards,
+            object : MemoryBoardAdapter.CardClickListener {
+                override fun onCardClicked(position: Int) {
+                    updateGameWithFlip(position)
+                }
+
+            })
         rvBoard.adapter = adapter
         rvBoard.setHasFixedSize(true)
-        rvBoard.layoutManager = GridLayoutManager(this,boardSize.getWidth())
-
+        rvBoard.layoutManager = GridLayoutManager(this, boardSize.getWidth())
     }
 
     private fun updateGameWithFlip(position: Int) {
         //Error checking
-        if(memoryGame.haveWonGame()){
+        if (memoryGame.haveWonGame()) {
             // Alert the user of an invalid move
-            Snackbar.make(clRoot, "You already won!",Snackbar.ANIMATION_MODE_SLIDE).show()
+            Snackbar.make(clRoot, "You already won!", Snackbar.ANIMATION_MODE_SLIDE).show()
             return
         }
-        if(memoryGame.isCardFaceUp(position)){
+        if (memoryGame.isCardFaceUp(position)) {
             // Alert the use of an invalid move
-            Snackbar.make(clRoot, "Invalid move!",Snackbar.ANIMATION_MODE_SLIDE).show()
+            Snackbar.make(clRoot, "Invalid move!", Snackbar.ANIMATION_MODE_SLIDE).show()
             return
         }
-         if(memoryGame.flipCard(position)){
-             Log.i(TAG,"Found a match! Num Paris Found : ${memoryGame.numPairsFound}")
-             val color = ArgbEvaluator().evaluate(
-                 memoryGame.numPairsFound.toFloat() / boardSize.getNumPairs(),
-                 ContextCompat.getColor(this,R.color.color_progress_none),
-                 ContextCompat.getColor(this,R.color.color_progress_full)
-             ) as Int
-             tvNumPairs.setTextColor(color)
-             tvNumPairs.text = "Paris: ${memoryGame.numPairsFound} / ${boardSize.getNumPairs()}"
-             if(memoryGame.haveWonGame()){
-                 Snackbar.make(clRoot, "You won! Congratulations",Snackbar.ANIMATION_MODE_SLIDE).show()
-             }
-         }
+        if (memoryGame.flipCard(position)) {
+            Log.i(TAG, "Found a match! Num Paris Found : ${memoryGame.numPairsFound}")
+            val color = ArgbEvaluator().evaluate(
+                memoryGame.numPairsFound.toFloat() / boardSize.getNumPairs(),
+                ContextCompat.getColor(this, R.color.color_progress_none),
+                ContextCompat.getColor(this, R.color.color_progress_full)
+            ) as Int
+            tvNumPairs.setTextColor(color)
+            tvNumPairs.text = "Paris: ${memoryGame.numPairsFound} / ${boardSize.getNumPairs()}"
+            if (memoryGame.haveWonGame()) {
+                Snackbar.make(clRoot, "You won! Congratulations", Snackbar.ANIMATION_MODE_SLIDE)
+                    .show()
+            }
+        }
         tvNumMoves.text = "Moves: ${memoryGame.getNumMoves()}"
-         adapter.notifyDataSetChanged()
+        adapter.notifyDataSetChanged()
     }
 }
