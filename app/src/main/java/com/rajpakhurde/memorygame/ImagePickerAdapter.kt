3064169886329c2aa8be8300b9ae5c93a2895dd8
Raj_Pakhurde <rajofficial4803@gmail.com>
2022-12-20 18:52:04 +0530

    Created ImageViews in CreateActivity and setup Recyclerview Adapter for Images list(CreateActivity)

---
+++ app/src/main/java/com/rajpakhurde/memorygame/ImagePickerAdapter.kt
@@ -0,0 +1,59 @@
+package com.rajpakhurde.memorygame
+
+import android.content.Context
+import android.media.Image
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.VideoView
+import androidx.recyclerview.widget.RecyclerView
+import com.rajpakhurde.memorygame.models.BoardSize
+import java.lang.Integer.min
+
+class ImagePickerAdapter(
+    private val context: Context,
+    private val imageUris: MutableList<Uri>,
+    private val boardSize: BoardSize
+) : RecyclerView.Adapter<ImagePickerAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.card_image, parent, false)
+        val cardWidth = parent.width / boardSize.getWidth()
+        val cardHeight = parent.height / boardSize.getHeight()
+        val cardSideLength = min(cardWidth, cardHeight)
+        val layoutParams = view.findViewById<ImageView>(R.id.ivCustomImage).layoutParams
+        layoutParams.width = cardSideLength
+        layoutParams.height = cardSideLength
+        return ViewHolder(view)
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        if (position < imageUris.size) {
+            holder.bind(imageUris[position])
+        } else {
+            holder.bind()
+        }
+    }
+
+    override fun getItemCount(): Int = boardSize.getNumPairs()
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val ivCustomImage = itemView.findViewById<ImageView>(R.id.ivCustomImage)
+        fun bind(uri: Uri) {
+            ivCustomImage.setImageURI(uri)
+            ivCustomImage.setOnClickListener(null)
+        }
+
+        fun bind() {
+            ivCustomImage.setOnClickListener {
+                // Launch intent for user to select image
+            }
+        }
+
+    }
+
+}
